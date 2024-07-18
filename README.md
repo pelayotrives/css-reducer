034bc6025b5f63cc2151e85919f84d071ae1d8f8
Pelayo Trives <pelayotrives@gmail.com>
2024-07-18 15:16:49 +0200

    feat: first version of script released.

---
+++ README.md
@@ -0,0 +1,26 @@
+# CSS Reducer
+
+This Python script reduce CSS syntax and distribute it in a single line to fill less space in the file.
+
+## Usage
+
+No extra modules nor libraries required.
+
+Modify the location of the input file and the name and location that you want it to have as output here:
+
+    ```
+    input_file_path = '../Desktop/example.css'
+    output_file_path = '../Desktop/example_compiled.css'
+    ```
+
+Run the script by executing
+
+    ```
+    python3 css-reducer.py
+    ```
+
+The script will process the file and get a reduced CSS file that can be useful on certain occasions.
+
+Enjoy!
+
+Created by Pelayo Trives.
---
+++ css_reducer.py
@@ -0,0 +1,41 @@
+import re
+
+def process_css(file_path):
+    with open(file_path, 'r') as file:
+        css_content = file.read()
+
+    comments = re.findall(r'/\*.*?\*/', css_content, flags=re.DOTALL)
+    css_content_no_comments = re.sub(r'/\*.*?\*/', '/*COMMENT_PLACEHOLDER*/', css_content, flags=re.DOTALL)
+
+    rules = re.findall(r'([^{]+){([^}]+)}', css_content_no_comments)
+
+    processed_rules = []
+    for selector, properties in rules:
+        selector = selector.strip()
+        properties_list = [prop.strip() for prop in properties.split(';') if prop.strip()]
+        properties = '; '.join(properties_list) + ';'
+        processed_rule = f"{selector} {{{properties}}}"
+        processed_rules.append(processed_rule)
+    
+    processed_css = '\n'.join(processed_rules)
+
+    for comment in comments:
+        processed_css = processed_css.replace('/*COMMENT_PLACEHOLDER*/', f'\n\n{comment}\n\n', 1)
+    
+    processed_css = re.sub(r'\n{3,}', '\n\n', processed_css)
+
+    return processed_css
+
+def save_processed_css(processed_css, output_file_path):
+    with open(output_file_path, 'w') as file:
+        file.write(processed_css)
+
+if __name__ == "__main__":
+    # Modify the paths to the desired files
+    input_file_path = '../Desktop/example.css'
+    output_file_path = '../Desktop/example_compiled.css'
+    
+    processed_css = process_css(input_file_path)
+    save_processed_css(processed_css, output_file_path)
+
+    print(f"Processed file saved in {output_file_path}")
